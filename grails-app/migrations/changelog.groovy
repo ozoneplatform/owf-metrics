e140017e65be45120e07b7fa88ff7dbdd2542e77
UberGeekxor <o3geekxor@gmail.com>
2013-10-10 16:33:52 -0400

    OP-3630: Add database scripts from previous release

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -7,6 +7,9 @@ databaseChangeLog = {
     //previous version change logs go here
     include file: 'changelog_5.0.0.groovy'
     include file: 'changelog_6.0.0.groovy'
+    include file: 'changelog_7.0.0.groovy'
+    include file: 'changelog_7.1.0.groovy'
+    include file: 'changelog_7.2.groovy'
 
     //include/exclude current version's change log based on existence of dbmBuildPreviousVersion
     if (!System.properties.dbmBuildPreviousVersion) {
