3a72d6deabb10c749fd366a17c0350f963abd915
UberGeekxor <o3geekxor@gmail.com>
2013-10-14 18:27:11 -0400

    Update for 7.3 release

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -10,6 +10,7 @@ databaseChangeLog = {
     include file: 'changelog_7.0.0.groovy'
     include file: 'changelog_7.1.0.groovy'
     include file: 'changelog_7.2.groovy'
+    include file: 'changelog_7.3.groovy'
 
     //include/exclude current version's change log based on existence of dbmBuildPreviousVersion
     if (!System.properties.dbmBuildPreviousVersion) {
